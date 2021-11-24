465728ba865718fdb093bedb72d3ea191f853045
Supetorus <66396555+Supetorus@users.noreply.github.com>
2021-11-24 15:11:16 -0700

    Revert "new selected color dark theme"
    This reverts commit af3105a22eb4c27acb1eb0ebcedec7f4ef24ff51.
    Fixes Themes losing formatting and the selectedTheme.

--- Sudoku/Theme.cs
+++ Sudoku/Theme.cs
@@ -12,106 +12,80 @@ namespace Sudoku
 		public Brush selectedColor;
 		public Brush matchingColor;
 		public Brush areaColor;
-		public Brush unselectedColor;
+		public Brush DefaultTileColor;
 		public Brush WrongColor;
 		public Brush RightColor;
 		public Brush BorderColor;
+		public Brush BackgroundColor;
+		public Brush DefaultText;
 
 		public Theme(Brush selectedColor, Brush matchingColor, Brush areaColor,
-			Brush unselectedColor, Brush WrongColor, Brush RightColor, Brush BorderColor)
+			Brush DefaultTileColor, Brush WrongColor, Brush RightColor, Brush BorderColor, Brush BackgroundColor, Brush DefaultText)
 		{
 			this.selectedColor = selectedColor;
 			this.matchingColor = matchingColor;
 			this.areaColor = areaColor;
-			this.unselectedColor = unselectedColor;
+			this.DefaultTileColor = DefaultTileColor;
 			this.WrongColor = WrongColor;
 			this.RightColor = RightColor;
 			this.BorderColor = BorderColor;
+			this.BackgroundColor = BackgroundColor;
+			this.DefaultText = DefaultText;
 		}
+
 		public readonly static Theme[] themes =
 		{
-			new Theme
+			new Theme // default / testing
 			(
-				new SolidColorBrush(Color.FromRgb(200, 200, 255)),
-				new SolidColorBrush(Color.FromRgb(255, 200, 200)),
-				new SolidColorBrush(Color.FromRgb(200, 200, 215)),
-				new SolidColorBrush(Color.FromRgb(200, 200, 200)),
-				new SolidColorBrush(Color.FromRgb(255, 000, 000)),
-				new SolidColorBrush(Color.FromRgb(000, 100, 200)),
-				new SolidColorBrush(Color.FromRgb(000, 000, 000))
+				new SolidColorBrush(Color.FromRgb(200, 200, 255)),// selected color
+				new SolidColorBrush(Color.FromRgb(255, 200, 200)),// matching color
+				new SolidColorBrush(Color.FromRgb(200, 200, 215)),// area color
+				new SolidColorBrush(Color.FromRgb(200, 200, 200)),// default tile color
+				new SolidColorBrush(Color.FromRgb(255, 000, 000)),// wrong color
+				new SolidColorBrush(Color.FromRgb(000, 100, 200)),// right color
+				new SolidColorBrush(Color.FromRgb(000, 000, 000)),// border color
+				new SolidColorBrush(Color.FromRgb(255, 255, 255)),// background color
+				new SolidColorBrush(Color.FromRgb(000, 000, 000)) // default text color
 			),
-			//dark theme
-				new Theme
+			new Theme // dark theme
 			(
-					//selected color
-				new SolidColorBrush(Color.FromRgb(1, 22, 39)),
-				//matching color
-				new SolidColorBrush(Color.FromRgb(65, 234, 200)),
-				//area color
-				new SolidColorBrush(Color.FromRgb(255, 255, 255)),
-				// unseleted color
-				new SolidColorBrush(Color.FromRgb(12, 15, 10)),
-				//wrong color
-				new SolidColorBrush(Color.FromRgb(255, 000, 000)),
-				//right color
-				new SolidColorBrush(Color.FromRgb(251, 255, 18)),
-				//bordercolor
-				new SolidColorBrush(Color.FromRgb(65, 234, 200))
+				new SolidColorBrush(Color.FromRgb(255, 032, 110)),// selected color
+				new SolidColorBrush(Color.FromRgb(065, 234, 200)),// matching color
+				new SolidColorBrush(Color.FromRgb(255, 255, 255)),// area color
+				new SolidColorBrush(Color.FromRgb(012, 015, 010)),// default tile color
+				new SolidColorBrush(Color.FromRgb(255, 000, 000)),// wrong color
+				new SolidColorBrush(Color.FromRgb(251, 255, 018)),// right color
+				new SolidColorBrush(Color.FromRgb(065, 234, 200)),// bordercolor
+				new SolidColorBrush(Color.FromRgb(077, 077, 077)),// background color
+				new SolidColorBrush(Color.FromRgb(145, 145, 145)) // default text color
+
 			),
-				//mocha theme
-				new Theme
+			new Theme // mocha theme
 			(
-					//selected color
-				new SolidColorBrush(Color.FromRgb(117, 79, 91)),
-				//matching color
-				new SolidColorBrush(Color.FromRgb(93, 73, 84)),
-				//area color
-				new SolidColorBrush(Color.FromRgb(249, 224, 217)),
-				// unseleted color
-				new SolidColorBrush(Color.FromRgb(125, 97, 103)),
-				//wrong color
-				new SolidColorBrush(Color.FromRgb(255, 000, 000)),
-				//right color
-				new SolidColorBrush(Color.FromRgb(230, 219, 208)),
-				//border color
-				new SolidColorBrush(Color.FromRgb(93, 73, 84))
+				new SolidColorBrush(Color.FromRgb(117, 079, 091)),// selected color
+				new SolidColorBrush(Color.FromRgb(093, 073, 084)),// matching color
+				new SolidColorBrush(Color.FromRgb(249, 224, 217)),// area color
+				new SolidColorBrush(Color.FromRgb(125, 097, 103)),// default tile color
+				new SolidColorBrush(Color.FromRgb(255, 000, 000)),// wrong color
+				new SolidColorBrush(Color.FromRgb(230, 219, 208)),// right color
+				new SolidColorBrush(Color.FromRgb(093, 073, 084)),// border color
+				new SolidColorBrush(Color.FromRgb(102, 075, 037)),// background color
+				new SolidColorBrush(Color.FromRgb(158, 127, 084)) // default text color
 			),
-				//the bluez theme
-				new Theme
+			new Theme // the bluez theme
 			(
-					//selected color
-				new SolidColorBrush(Color.FromRgb(231, 236, 239)),
-				//matching color
-				new SolidColorBrush(Color.FromRgb(139, 140, 137)),
-				//area color
-				new SolidColorBrush(Color.FromRgb(96, 150, 186)),
-				// unseleted color
-				new SolidColorBrush(Color.FromRgb(36, 76, 119)),
-				//wrong color
-				new SolidColorBrush(Color.FromRgb(255, 000, 000)),
-				//right color
-				new SolidColorBrush(Color.FromRgb(163, 206, 241)),
-				//border color
-				new SolidColorBrush(Color.FromRgb(139, 140, 137))
-			),
-				//the neon theme
-				new Theme
-			(
-					//selected color
-				new SolidColorBrush(Color.FromRgb(255, 254, 255)),
-				//matching color
-				new SolidColorBrush(Color.FromRgb(250, 255, 0)),
-				//area color
-				new SolidColorBrush(Color.FromRgb(255, 1, 251)),
-				// unseleted color
-				new SolidColorBrush(Color.FromRgb(2, 196, 234)),
-				//wrong color
-				new SolidColorBrush(Color.FromRgb(255, 000, 000)),
-				//right color
-				new SolidColorBrush(Color.FromRgb(163, 206, 241)),
-				//border color
-				new SolidColorBrush(Color.FromRgb(250, 255, 0))
+				new SolidColorBrush(Color.FromRgb(231, 236, 239)),// selected color
+				new SolidColorBrush(Color.FromRgb(139, 140, 137)),// matching color
+				new SolidColorBrush(Color.FromRgb(096, 150, 186)),// area color
+				new SolidColorBrush(Color.FromRgb(036, 076, 119)),// default tile color
+				new SolidColorBrush(Color.FromRgb(255, 000, 000)),// wrong color
+				new SolidColorBrush(Color.FromRgb(163, 206, 241)),// right color
+				new SolidColorBrush(Color.FromRgb(139, 140, 137)),// border color
+				new SolidColorBrush(Color.FromRgb(019, 081, 148)),// background color
+				new SolidColorBrush(Color.FromRgb(071, 135, 204)) // default text color
 			)
 		};
+
+		public static Theme selectedTheme = themes[1];
 	}
 }
