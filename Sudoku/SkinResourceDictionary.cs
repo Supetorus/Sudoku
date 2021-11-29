d3e48eb18866ae4b98ac44a8ba3751cb5131bfd9
Supetorus <66396555+Supetorus@users.noreply.github.com>
2021-11-29 05:50:57 -0700

    Implements Dark Mode

--- Sudoku/SkinResourceDictionary.cs
+++ Sudoku/SkinResourceDictionary.cs
@@ -7,35 +7,47 @@ using System.Windows;
 
 namespace Sudoku
 {
-	public class SkinResourceDictionary : ResourceDictionary
-	{
-		private Uri _defaultSource;
-		private Uri _darkSource;
+    public class SkinResourceDictionary : ResourceDictionary
+    {
+        private Uri _defaultSource;
+        private Uri _darkSource;
 
-		public Uri DefaultSource
-		{
-			get { return _defaultSource; }
-			set
-			{
-				_defaultSource = value;
-				UpdateSource();
-			}
-		}
-		public Uri DarkSource
-		{
-			get { return _darkSource; }
-			set
-			{
-				_darkSource = value;
-				UpdateSource();
-			}
-		}
+        public Uri DefaultSource
+        {
+            get { return _defaultSource; }
+            set
+            {
+                _defaultSource = value;
+                UpdateSource();
+            }
+        }
+        public Uri DarkSource
+        {
+            get { return _darkSource; }
+            set
+            {
+                _darkSource = value;
+                UpdateSource();
+            }
+        }
 
-		private void UpdateSource()
-		{
-			var val = App.Skin == Skin.Default ? DefaultSource : DarkSource;
-			if (val != null && base.Source != val)
-				base.Source = val;
-		}
-	}
+        private void UpdateSource()
+        {
+            Uri val;
+            switch (App.Skin)
+            {
+                case Skin.Default:
+                    val = DefaultSource;
+                    break;
+                case Skin.Dark:
+                    val = DarkSource;
+                    break;
+                default:
+                    val = DefaultSource;
+                    break;
+            }
+            if (val != null && base.Source != val)
+                base.Source = val;
+        }
+    }
 }
